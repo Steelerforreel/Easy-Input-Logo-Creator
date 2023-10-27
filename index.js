953e1488c1ef338ca864bdae8da7ac83ef1dd372
Steelerforreel <reelnicholas@gmail.com>
2023-10-26 22:05:25 -0400

    Added all files necessary for creating program

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { createLogo } = require("./lib/createLogo");
+const { createShape } = require("./lib/createShape");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "Name",
+      message: "Enter a name that has at least 1-3 characters.",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: `Enter a color keyword or a hexadecimal number`,
+    },
+    {
+      type: "list",
+      name: "logoShape",
+      message: `Please choose logo shape`,
+      choices: ["Circle", "Triangle", "Square"],
+      },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: `Enter a color keyword or a hexadecimal number`,
+    },
+    
+])
+  .then((data) => {
+    const path = "./output/Logo.svg";
+    const logo = createShape(data);
+
+    fs.writeFile(path, createLogo(logo), (err) =>
+      err ? console.error(err) : console.log("Your Logo.svg has been created!")
+    );
+  })
+  .catch((err) => console.error(err));
